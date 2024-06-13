cc00f1eb2e64349c50bf6e017cd6a482b9b6e9c4
TheFluffyAsian <neilhbaker@gmail.com>
2024-06-13 12:43:06 +0100

    Initial unit test working

--- SpaceInvaders.Tests/UnitTest1.cs
+++ SpaceInvaders.Tests/UnitTest1.cs
@@ -1,4 +1,4 @@
-using SpaceInvaders.Avalonia;
+using SpaceInvaders.Avalonia.ViewModels;
 
 namespace SpaceInvaders.Tests;
 
@@ -7,6 +7,11 @@ public class UnitTest1
     [Fact]
     public void Test1()
     {
-        //var thing = new MainViewModel();
+        var thing = new MainWindowViewModel();
+        
+        var expectedGreeting = "Welcome to Avalonia!";
+
+        Assert.Equal(expectedGreeting, thing.Greeting);
+        
     }
 }
